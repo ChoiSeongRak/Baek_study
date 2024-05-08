3abe072b6a1ad9a39e18ce6c72f01baed8d4cd67
ChoiSeongRak <164332446+ChoiSeongRak@users.noreply.github.com>
2024-05-08 21:27:55 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+g = int(input())
+h = int(input())
+i = int(input())
+ma = [a,b,c,d,e,f,g,h,i]
+print(max(ma))
+print(ma.index(max(ma))+1)
+
+
+
